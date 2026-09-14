b86e35654d0a994a17c4f626c2399eef1aefeebc
KaranBaro <karanbaro.growify@gmail.com>
2026-09-14 16:41:05 +0530

    More changed to image

--- extensions/checkout-upsell/src/Checkout.jsx
+++ extensions/checkout-upsell/src/Checkout.jsx
@@ -695,14 +695,14 @@ function ProductCard({
                   src={product.image}
                   alt={product.title}
                   inlineSize="100%"
-                  aspectRatio="3/5"
-                  objectFit="cover"
+                  aspectRatio="4/5"
+                  objectFit="contain"
                 />
               ) : (
                 <s-box
                   background="subdued"
                   inlineSize="100%"
-                  aspectRatio="3/5"
+                  aspectRatio="4/5"
                 />
               )}
 
